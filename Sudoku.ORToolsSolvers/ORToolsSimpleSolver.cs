0f5f46f14e2dcc3ed981891f1a7e3b0b231202ab
Aylingithub <aylin.kelle@edu.ece.fr>
2025-01-28 14:37:04 +0100

    Initialisation du solver ORTOOLS vide

---
+++ Sudoku.ORToolsSolvers/ORToolsSimpleSolver.cs
@@ -0,0 +1,13 @@
+﻿using Sudoku.Shared;
+
+namespace Sudoku.ORToolsSolvers ;
+
+
+public class ORToolsSimpleSolvers : ISudokuSolver
+{
+       public SudokuGrid Solve(SudokuGrid s)
+    {
+        return s;
+    }
+
+}
