91b34639c77e8ee0a344d4abbe62da581c4580b8
machadziri <macha.dziri@gmail.com>
2025-02-01 13:17:59 +0100

    Initialisation du solver Color Graph vide

---
+++ Sudoku.ColorGraphSolvers/ColorGraphSimpleSolver.cs
@@ -0,0 +1,11 @@
+﻿using Sudoku.Shared;
+
+namespace Sudoku.ColorGraphSolvers;
+
+public class ColorGraphSimpleSolver : ISudokuSolver
+{
+    public SudokuGrid Solve(SudokuGrid s)
+    {
+        return s;
+    }
+}
