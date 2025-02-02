a629e5cc0d8aeceba54c910670f933fe75832c78
Etiennecmoi <96126239+Etiennecmoi@users.noreply.github.com>
2025-02-02 14:49:31 +0100

    Resolution problème grille

--- Sudoku.RecuitSimule.Solvers/RecuitSimuleSolverSimple.cs
+++ Sudoku.RecuitSimule.Solvers/RecuitSimuleSolverSimple.cs
@@ -11,31 +11,49 @@ namespace Sudoku.RecuitSimule.Solvers
 
         public SudokuGrid Solve(SudokuGrid s)
         {
+            // Cloner la grille d'origine et préparer le masque des cellules fixes
             int[,] grid = (int[,])s.Cells.Clone();
-            SolveWithSimulatedAnnealing(grid);
+            bool[,] fixedCells = new bool[9, 9];
+            
+            // Remplir le masque des cellules fixes (cellules non vides initiales)
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    fixedCells[i, j] = grid[i, j] != 0;
+                }
+            }
+            
+            SolveWithSimulatedAnnealing(grid, fixedCells);
             return new SudokuGrid { Cells = grid };
         }
 
-        private void SolveWithSimulatedAnnealing(int[,] grid)
+        private void SolveWithSimulatedAnnealing(int[,] grid, bool[,] fixedCells)
         {
+            // Paramètres du recuit simulé
             double temperature = 1.0;
             double coolingRate = 0.995;
             int iterations = 100000;
 
-            int[,] currentSolution = InitializeSolution(grid);
+            // Initialisation de la solution en respectant les cellules fixes
+            int[,] currentSolution = InitializeSolution(grid, fixedCells);
             int[,] bestSolution = (int[,])currentSolution.Clone();
             int currentEnergy = ComputeEnergy(currentSolution);
             int bestEnergy = currentEnergy;
 
             for (int i = 0; i < iterations; i++)
             {
-                int[,] newSolution = MakeSmallChange(currentSolution);
+                // Créer une nouvelle solution modifiée (en respectant les cellules fixes)
+                int[,] newSolution = MakeSmallChange(currentSolution, fixedCells);
                 int newEnergy = ComputeEnergy(newSolution);
 
+                // Accepter la nouvelle solution si elle est meilleure ou selon une probabilité
                 if (newEnergy < bestEnergy || AcceptWorseSolution(currentEnergy, newEnergy, temperature))
                 {
                     currentSolution = newSolution;
                     currentEnergy = newEnergy;
+                    
+                    // Mettre à jour la meilleure solution trouvée
                     if (currentEnergy < bestEnergy)
                     {
                         bestSolution = (int[,])currentSolution.Clone();
@@ -43,25 +61,31 @@ namespace Sudoku.RecuitSimule.Solvers
                     }
                 }
 
+                // Refroidir le système
                 temperature *= coolingRate;
-                if (bestEnergy == 0) break;
+                if (bestEnergy == 0) break; // Solution optimale trouvée
             }
 
+            // Copier la meilleure solution dans la grille de résultat
             Array.Copy(bestSolution, grid, grid.Length);
         }
 
-        private int[,] InitializeSolution(int[,] grid)
+        // Initialise une solution valide en remplissant les cellules vides sans conflits dans les lignes
+        private int[,] InitializeSolution(int[,] grid, bool[,] fixedCells)
         {
             int[,] solution = (int[,])grid.Clone();
             for (int i = 0; i < 9; i++)
             {
+                // Trouver les nombres manquants dans la ligne
                 HashSet<int> missingNumbers = new HashSet<int>(Enumerable.Range(1, 9));
                 for (int j = 0; j < 9; j++)
                 {
                     if (solution[i, j] != 0)
                         missingNumbers.Remove(solution[i, j]);
                 }
-                foreach (int j in Enumerable.Range(0, 9).Where(j => solution[i, j] == 0))
+                
+                // Remplir les cellules non fixes avec les nombres manquants
+                foreach (int j in Enumerable.Range(0, 9).Where(j => !fixedCells[i, j]))
                 {
                     if (missingNumbers.Count > 0)
                     {
@@ -74,17 +98,31 @@ namespace Sudoku.RecuitSimule.Solvers
             return solution;
         }
 
+        // Calcule l'énergie (nombre de conflits) de la grille
         private int ComputeEnergy(int[,] grid)
         {
             int conflicts = 0;
+            
+            // Conflits dans les lignes et colonnes
             for (int i = 0; i < 9; i++)
             {
-                conflicts += CountConflicts(grid, i, true);
-                conflicts += CountConflicts(grid, i, false);
+                conflicts += CountConflicts(grid, i, true); // Lignes
+                conflicts += CountConflicts(grid, i, false); // Colonnes
             }
+            
+            // Conflits dans les sous-grilles 3x3
+            for (int i = 0; i < 9; i += 3)
+            {
+                for (int j = 0; j < 9; j += 3)
+                {
+                    conflicts += CountSubgridConflicts(grid, i, j);
+                }
+            }
+            
             return conflicts;
         }
 
+        // Compte les conflits dans une ligne ou une colonne
         private int CountConflicts(int[,] grid, int index, bool isRow)
         {
             HashSet<int> seen = new HashSet<int>();
@@ -98,16 +136,58 @@ namespace Sudoku.RecuitSimule.Solvers
             return conflicts;
         }
 
-        private int[,] MakeSmallChange(int[,] grid)
+        // Compte les conflits dans une sous-grille 3x3
+        private int CountSubgridConflicts(int[,] grid, int startRow, int startCol)
+        {
+            HashSet<int> seen = new HashSet<int>();
+            int conflicts = 0;
+            for (int i = startRow; i < startRow + 3; i++)
+            {
+                for (int j = startCol; j < startCol + 3; j++)
+                {
+                    int num = grid[i, j];
+                    if (seen.Contains(num)) conflicts++;
+                    seen.Add(num);
+                }
+            }
+            return conflicts;
+        }
+
+        // Échange deux cellules non fixes dans la même ligne
+        private int[,] MakeSmallChange(int[,] grid, bool[,] fixedCells)
         {
             int[,] newGrid = (int[,])grid.Clone();
             int row = random.Next(0, 9);
-            int col1 = random.Next(0, 9);
-            int col2 = random.Next(0, 9);
+            
+            // Collecter les indices des cellules modifiables dans cette ligne
+            List<int> swappableColumns = new List<int>();
+            for (int j = 0; j < 9; j++)
+            {
+                if (!fixedCells[row, j]) // Ne considère que les cellules non fixes
+                    swappableColumns.Add(j);
+            }
+            
+            // S'il y a moins de 2 cellules à échanger, retourner la grille originale
+            if (swappableColumns.Count < 2)
+                return newGrid;
+            
+            // Choisir deux cellules aléatoirement
+            int colIndex1 = random.Next(swappableColumns.Count);
+            int colIndex2;
+            do
+            {
+                colIndex2 = random.Next(swappableColumns.Count);
+            } while (colIndex1 == colIndex2);
+            
+            // Échanger les valeurs
+            int col1 = swappableColumns[colIndex1];
+            int col2 = swappableColumns[colIndex2];
             (newGrid[row, col1], newGrid[row, col2]) = (newGrid[row, col2], newGrid[row, col1]);
+            
             return newGrid;
         }
 
+        // Détermine si on accepte une solution moins bonne
         private bool AcceptWorseSolution(int currentEnergy, int newEnergy, double temperature)
         {
             if (newEnergy < currentEnergy) return true;
@@ -116,5 +196,3 @@ namespace Sudoku.RecuitSimule.Solvers
         }
     }
 }
-
-
